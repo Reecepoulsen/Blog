9eeedc2cf2638922304d729b4e2af85271a4fa23
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-01 14:40:50 -0600

    removed comment blocks

--- ServiceNow Custom Components Explained/code/src/x-961977-color-picker/index.js
+++ ServiceNow Custom Components Explained/code/src/x-961977-color-picker/index.js
@@ -1,50 +1,27 @@
-/******************************************************
- * Section 1: Import statements
- ******************************************************/
 import { createCustomElement, actionTypes } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
 
-/******************************************************
- * Section 2: Setup
- * - action types
- * - initial state
- ******************************************************/
 const { COMPONENT_CONNECTED, COMPONENT_PROPERTY_CHANGED, COMPONENT_DOM_READY } =
 	actionTypes;
 const initialState = {};
 
-/******************************************************
- * Section 3: Define the component
- ******************************************************/
 const view = (state, { updateState }) => {
 	return <div>Hello World!</div>;
 };
 
-/******************************************************
- * Section 4: Define action handlers
- ******************************************************/
-const actionHandlers = {};
-
-/******************************************************
- * Section 5: Define dispatches
- ******************************************************/
-const dispatches = {};
-
-/******************************************************
- * Section 6: Export Properties
- ******************************************************/
 export const properties = {};
 
-/******************************************************
- * Section 7: Provide component build information
- ******************************************************/
+const actionHandlers = {};
+
+const dispatches = {};
+
 createCustomElement("x-961977-color-picker", {
-	renderer: { type: snabbdom },
-	view,
-	styles,
 	initialState,
+	view,
+	properties,
 	actionHandlers,
 	dispatches,
-	properties,
+	styles,
+	renderer: { type: snabbdom },
 });

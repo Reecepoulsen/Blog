cef39f4e45a923e57610c5442706ace55de0049e
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-03 15:26:21 -0600

    Finished blog example

--- ServiceNow Custom Components Explained/code/src/x-961977-dark-mode-toggle/index.js
+++ ServiceNow Custom Components Explained/code/src/x-961977-dark-mode-toggle/index.js
@@ -1,34 +1,70 @@
-import { createCustomElement, actionTypes } from "@servicenow/ui-core";
+import { createCustomElement } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
 
-const view = ({ darkMode }, { updateState }) => {
-	console.log("Component rendered");
+// Declare the initial state of the component
+const initialState = {
+	darkMode: false,
+};
+
+// Define the view
+const view = ({ darkMode, properties }, { updateState, dispatch }) => {
+	const doSomething = () => {
+		dispatch("MY_FIRST_ACTION", { data: "I'm the payload data" });
+	};
+
 	return (
-		<div
+		<button
 			id="container"
 			className={darkMode ? "dark" : "light"}
 			on-click={() => {
-				console.log(`Turning Dark mode ${darkMode ? "off" : "on"}`);
+				doSomething();
 				updateState({
 					darkMode: !darkMode,
 				});
 			}}
 		>
-			<div id="text">{darkMode ? "Dark" : "Light"}</div>
-		</div>
+			{properties.buttonText}
+		</button>
 	);
 };
 
-createCustomElement("x-961977-dark-mode-toggle", {
-	initialState: {
-		darkMode: false,
+// Define the properties
+const properties = {
+	buttonText: {
+		default: "Click Me!",
+		schema: { type: "string" },
 	},
+};
+
+// Define action handlers
+const actionHandlers = {
+	MY_FIRST_ACTION: ({ action: { payload } }) => {
+		console.log("MY_FIRST_ACTION payload:", payload.data);
+	},
+};
+
+// Define event handlers
+const eventHandlers = [
+	{
+		events: ["click"], // Accepts a list of events
+		effect({
+			action: {
+				payload: { event, host },
+			},
+		}) {
+			console.log("Native DOM JS Event:", event); // Native DOM JS Event object
+		},
+	},
+];
+
+// Render the component
+createCustomElement("x-961977-dark-mode-toggle", {
+	initialState,
 	view,
-	properties: {},
-	actionHandlers: {},
-	eventHandlers: [],
-	dispatches: {},
+	properties,
+	actionHandlers,
+	eventHandlers,
 	styles,
 	renderer: { type: snabbdom },
 });
